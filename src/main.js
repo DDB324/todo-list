a0b14f3890ae495aa76d4be667bb8dfdd790bc16
ddb324 <jiangdaoran@163.com>
2023-04-05 10:53:33 +0800

    complete page functions

--- src/main.js
+++ src/main.js
@@ -14,29 +14,53 @@ function downDrawer() {
     document.querySelector(".todoFooter").style.bottom = "-300px";
 }
 
-function clickTask() {
+function clickTaskPage() {
     document.getElementsByClassName("introduction")[0].innerHTML = "<div class=\"iconfont icon-home-fill\"></div><span>任务</span>";
     render();
     closeDrawer();
 }
 
-function clickImportant() {
+function clickImportantPage() {
     document.getElementsByClassName("introduction")[0].innerHTML = "<div class=\"iconfont icon-shiwujiaoxing\"></div><span>重要</span>";
     render();
     closeDrawer();
 }
 
-function clickComplete() {
+function clickCompletePage() {
     document.getElementsByClassName("introduction")[0].innerHTML = "<div class=\"iconfont icon-zhengque\"></div><span>完成</span>";
     render();
     closeDrawer();
 }
 
 function markComplete() {
+    completeTodoItem();
     downDrawer();
 }
 
+function completeTodoItem() {
+    let todoListArray = getLocalStorage();
+    const index = todoListArray.findIndex(todoItem => todoItem.title === currentOnClickTodoItemTitle)
+    if (index != -1) {
+        todoListArray[index].done = true;
+    }
+    setLocalStorage(todoListArray);
+    currentOnClickTodoItemTitle = "";
+    render();
+}
+
+function importantTodoItem() {
+    let todoListArray = getLocalStorage();
+    const index = todoListArray.findIndex(todoItem => todoItem.title === currentOnClickTodoItemTitle)
+    if (index != -1) {
+        todoListArray[index].important = true;
+    }
+    setLocalStorage(todoListArray);
+    currentOnClickTodoItemTitle = "";
+    render();
+}
+
 function markImportant() {
+    importantTodoItem()
     downDrawer();
 }
 
@@ -44,13 +68,21 @@ function deleteTask() {
     downDrawer();
 }
 
-function onComplete() {
-    console.log(1);
-}
-function onImportant() {
-    console.log(2);
+function onComplete(event) {
+    currentOnClickTodoItemTitle = event.target.parentNode.innerText;
+    completeTodoItem();
 }
 
+function onImportant(event) {
+    currentOnClickTodoItemTitle = event.target.parentNode.innerText;
+    importantTodoItem();
+}
+
+function onClickTodoItem(event) {
+    currentOnClickTodoItemTitle = event.target.innerText;
+    upDrawer();
+}
+let currentOnClickTodoItemTitle;
 let myInput = document.getElementsByClassName("myInput")[0];
 
 // 监听输入框的按键事件
@@ -127,12 +159,22 @@ const render = () => {
         setEmptyIconDisappear();
         filteredTodoListFromLocalStorage.forEach((item) => {
             const wujiaoxing = item.important === false ? "kongwujiaoxing" : "shiwujiaoxing";
-            const todoLi = `<li class="todo">
-                                <div class="todoEnter" onclick="onComplete()"></div>
-                                <span onclick="upDrawer()">${item.title}</span>
-                                <div class="iconfont icon-${wujiaoxing}" onclick="onImportant()"> </div>
-                            </li>
-                            `
+            let todoLi;
+            if (item.done) {
+                todoLi = `<li class="todo">
+                <div class="iconfont icon-zhengque""></div>
+                <span onclick="onClickTodoItem(event)" style="text-decoration:line-through">${item.title}</span>
+                <div class="iconfont icon-${wujiaoxing}" onclick="onImportant(event)"> </div>
+            </li>
+            `
+            } else {
+                todoLi = `<li class="todo">
+                <div class="todoEnter" onclick="onComplete(event)"></div>
+                <span onclick="onClickTodoItem(event)">${item.title}</span>
+                <div class="iconfont icon-${wujiaoxing}" onclick="onImportant(event)"> </div>
+            </li>
+            `
+            }
             currentPageTodosHtml += todoLi;
         })
         setCurrentPageTodosHtml(currentPageTodosHtml);
